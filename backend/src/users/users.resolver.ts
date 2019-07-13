1923bfbcfc6affcc870a395e36c9ce5e1099f27f
Johannes Heilmann <johannes_heilmann@t-online.de>
2019-07-13 22:31:02 +0200

    Removed sign up response TO

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -2,7 +2,6 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { LoginResponseTo } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
-import { SignUpResponseTo } from './dto/sign-up-response.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { User } from './models/User';
 import { UsersService } from './users.service';
@@ -18,8 +17,8 @@ export class UserResolver {
     return this.userService.findAll();
   }
 
-  @Mutation((returns) => SignUpResponseTo)
-  async signUp(@Args('signUpDto') signUpDto: SignUpDto): Promise<SignUpResponseTo> {
+  @Mutation((returns) => LoginResponseTo)
+  async signUp(@Args('signUpDto') signUpDto: SignUpDto): Promise<LoginResponseTo> {
     const user = await this.userService.signUp(signUpDto);
     user.isHost = true;
     user.isGuest = true;
