8190fa7ddfd47c6b95f9f4756b37d4de2487fb61
Felix Eckert <felix.eckert@live.de>
2019-07-19 13:55:54 +0200

    Query list

--- frontend/src/SearchResults/SearchResults.tsx
+++ frontend/src/SearchResults/SearchResults.tsx
@@ -1,30 +1,80 @@
 import React from 'react';
 
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import { GridContainerMD, Section } from '../StyledComponents/StyledBasicItems';
-import { StyledSearchResultCard } from './SearchResultsStyle';
+import { Space, StyledSearchResultCard } from './SearchResultsStyle';
 
-const imageArray: [string] = [
-  'https://user-images.githubusercontent.com/3694962/44681202-478f4280-aa40-11e8-8d6f-f8e3133ab067.png',
-];
+const ALL_ACCOMMODATIONS_QUERY = gql`
+  query accommodations {
+    accommodations {
+      _id
+      isActive
+      country
+      city
+      streetName
+      streetNumber
+      zipCode
+      description
+      district
+      numberOfBeds
+      pictures
+    }
+  }
+`;
+
+interface Data {
+  accommodations: Array<{
+    _id: string;
+    isActive: boolean;
+    country: string;
+    city: string;
+    streetName: string;
+    streetNumber: string;
+    zipCode: string;
+    description: string;
+    district: string;
+    numberOfBeds: number;
+    pictures: [string];
+  }>;
+}
 
 class SearchResults extends React.Component {
   render() {
     return (
       <Section>
-        <GridContainerMD>
-          <StyledSearchResultCard
-            imageApartment={imageArray}
-            descriptionApartment="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-            numberOfBeds={3}
-            district="Schwabing"
-            name="Peter E."
-            descriptionHost="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-            hometown="Munich"
-            favoriteFood="Sushi"
-            positiveReviews={7}
-            negativeReviews={2}
-          />
-        </GridContainerMD>
+        <Query<Data, {}> query={ALL_ACCOMMODATIONS_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <p>Loading...</p>;
+            }
+            if (error) {
+              return <p>Error :(</p>;
+            }
+            if (!data) {
+              return <p>No Data :(</p>;
+            }
+
+            return data.accommodations.map((accommodation) => (
+              <GridContainerMD>
+                <StyledSearchResultCard
+                  key={accommodation._id}
+                  imageApartment={accommodation.pictures}
+                  descriptionApartment={accommodation.description}
+                  numberOfBeds={accommodation.numberOfBeds}
+                  district={accommodation.district}
+                  name="Peter E."
+                  descriptionHost="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                  hometown="Munich"
+                  favoriteFood="Sushi"
+                  positiveReviews={7}
+                  negativeReviews={2}
+                />
+                <Space />
+              </GridContainerMD>
+            ));
+          }}
+        </Query>
       </Section>
     );
   }
--- frontend/src/SearchResults/SearchResultsStyle.tsx
+++ frontend/src/SearchResults/SearchResultsStyle.tsx
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
 import SearchResultCard from './SearchResultCard/SearchResultCard';
 
-export const StyledSearchResultCard = styled(SearchResultCard)``;
+export const StyledSearchResultCard = styled(SearchResultCard)`
+  margin-bottom: 20px;
+`;
+
+export const Space = styled.div`
+  height: 20px;
+`;
