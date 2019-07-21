c43b4d445bdd6f9b02d6767fbc29c18911180039
ge72get <ge72get@mytum.de>
2019-07-21 08:28:29 +0200

    Update create-tripreport.dto.ts

---
+++ backend/src/tripReport/dto/create-tripReport.dto.ts
@@ -0,0 +1,27 @@
+import { Field, InputType } from 'type-graphql';
+import { IsNotEmpty } from 'class-validator';
+import { IsRoleType } from '../../common/validators/roleTypeValidator';
+import {RoleType} from '../../rating/models/Rating';
+
+@InputType()
+export class CreateTripReportDto {
+
+  @Field({ nullable: true })
+  _id?: string; //tslint:disable-line
+
+  @Field()
+  request!: string;
+
+  @Field((type) => RoleType)
+  @IsRoleType('Given ($value) is not a Role Type (MEAL or ACCOMMODATION)!')
+  receiverRole!: RoleType;
+
+  @Field()
+  @IsNotEmpty()
+  description!: string;
+
+  @Field((type) => [String])
+  pictures!: string[];
+
+}
+
