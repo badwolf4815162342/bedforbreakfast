9b809b65cc51a59dc25d1f3ab5c2534d34a2f645
Johannes Heilmann <johannes.heilmann@maibornwolff.de>
2019-07-21 23:20:47 +0200

    Added answer seen notification.

--- frontend/src/NavigationElements/Header/Notifications/NotificationList.tsx
+++ frontend/src/NavigationElements/Header/Notifications/NotificationList.tsx
@@ -1,10 +1,10 @@
 import { MenuItem } from '@material-ui/core';
 import gql from 'graphql-tag';
 import React from 'react';
-import { Query } from 'react-apollo';
+import { Mutation, Query } from 'react-apollo';
 import { HeaderLink } from '../HeaderStyle';
 
-const RECEIVE_REQUESTED_REQUEST = gql`
+const RECEIVED_REQUESTED_REQUEST = gql`
   query {
     receivedRequestedRequests {
       _id
@@ -15,7 +15,27 @@ const RECEIVE_REQUESTED_REQUEST = gql`
   }
 `;
 
-interface Data {
+const PROPOSED_UNSEE_ANSWERED_REQUESTS = gql`
+  query {
+    proposedUnseeAnsweredRequests {
+      _id
+      requestStatus
+      receiver {
+        firstName
+      }
+    }
+  }
+`;
+
+const UPDATE_AS_SEEN = gql`
+  mutation updateRequestAsSeen($_id: String!) {
+    updateRequestAsSeen(requestSeenDto: { _id: $_id }) {
+      _id
+    }
+  }
+`;
+
+interface ReceivedRequestsData {
   receivedRequestedRequests: [
     {
       _id: string;
@@ -24,30 +44,86 @@ interface Data {
   ];
 }
 
+interface ProposedUnseeAnsweredRequestsData {
+  proposedUnseeAnsweredRequests: [
+    {
+      _id: string;
+      requestStatus: string;
+      receiver: {
+        firstName: string;
+      };
+    },
+  ];
+}
+
 class NotificationList extends React.Component<{ onClick: () => void }, {}> {
   render() {
     return (
-      <Query<Data, {}> query={RECEIVE_REQUESTED_REQUEST}>
-        {({ loading, error, data }) => {
-          if (loading) {
-            return <p>Loading...</p>;
-          }
-          if (error) {
-            return <p>Error.</p>;
-          }
-          if (data && data.receivedRequestedRequests && data.receivedRequestedRequests.length > 0) {
-            return data.receivedRequestedRequests.map((request) => (
-              <HeaderLink key={request._id} to="/requests">
-                <MenuItem onClick={this.props.onClick}>
-                  You got a new request from {request.proposer.firstName}
-                </MenuItem>
-              </HeaderLink>
-            ));
-          } else {
-            return <p>No data.</p>;
-          }
-        }}
-      </Query>
+      <>
+        <Query<ReceivedRequestsData, {}> query={RECEIVED_REQUESTED_REQUEST}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <MenuItem onClick={this.props.onClick}>Loading...</MenuItem>;
+            }
+            if (error) {
+              return <MenuItem onClick={this.props.onClick}>Error.</MenuItem>;
+            }
+            if (data && data.receivedRequestedRequests && data.receivedRequestedRequests.length > 0) {
+              return data.receivedRequestedRequests.map((request) => (
+                <HeaderLink key={request._id} to="/requests">
+                  <MenuItem onClick={this.props.onClick}>
+                    You got a new request from {request.proposer.firstName}
+                  </MenuItem>
+                </HeaderLink>
+              ));
+            } else {
+              return <MenuItem onClick={this.props.onClick}>No open requests</MenuItem>;
+            }
+          }}
+        </Query>
+        <Query<ProposedUnseeAnsweredRequestsData, {}> query={PROPOSED_UNSEE_ANSWERED_REQUESTS}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <MenuItem onClick={this.props.onClick}>Loading...</MenuItem>;
+            }
+            if (error) {
+              return <MenuItem onClick={this.props.onClick}>Error</MenuItem>;
+            }
+            if (data && data.proposedUnseeAnsweredRequests && data.proposedUnseeAnsweredRequests.length > 0) {
+              return (
+                <Mutation mutation={UPDATE_AS_SEEN}>
+                  {(
+                    updateSeenMutation: (arg0: {
+                      variables: {
+                        _id: string;
+                      };
+                    }) => void,
+                    { mutationLoading, mutationError }: any,
+                  ) => (
+                    <>
+                      {mutationLoading && <p>Loading...</p>}
+                      {mutationError && <p>Error.</p>}
+                      {data.proposedUnseeAnsweredRequests.map((request) => (
+                        <MenuItem
+                          key={request._id}
+                          onClick={() => {
+                            this.props.onClick();
+                            updateSeenMutation({ variables: { _id: request._id } });
+                          }}
+                        >
+                          {request.receiver.firstName} {request.requestStatus} your request.
+                        </MenuItem>
+                      ))}
+                    </>
+                  )}
+                </Mutation>
+              );
+            } else {
+              return <MenuItem onClick={this.props.onClick}>No new answers</MenuItem>;
+            }
+          }}
+        </Query>
+      </>
     );
   }
 }
