f56e5d7668aef7bbf5d748736c13243f418ff2f3
Felix Eckert <felix.eckert@live.de>
2019-07-21 23:19:09 +0200

    Delete the items from Requests plus styling

--- frontend/src/Requests/Request/Request.tsx
+++ frontend/src/Requests/Request/Request.tsx
@@ -1,30 +1,78 @@
 import React from 'react';
+import {
+  AuthorPic,
+  AuthorPicBox,
+  ButtonContainer,
+  ContactContainer,
+  DescriptionContainer,
+  HeaderLink,
+  RequestCard,
+  RequestContainer,
+} from './RequestStyle';
 
 // import {} from './RequestStyle';
-import { Icon, IconButton, Paper } from '@material-ui/core';
+import { Icon, IconButton } from '@material-ui/core';
 import Moment from 'react-moment';
 
 const Request: React.FC<{
   start: string;
   end: string;
   description: string;
-  proposer: { firstName: string; lastName: string };
+  proposer: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    profilePicture: string;
+    email: string;
+    phoneNumber: string;
+  };
   onAccept: (accepted: boolean) => void;
-}> = ({ start, end, description, proposer: { firstName, lastName }, onAccept }) => {
+}> = ({
+  start,
+  end,
+  description,
+  proposer: { _id, firstName, lastName, profilePicture, email, phoneNumber },
+  onAccept,
+}) => {
   return (
-    <Paper>
-      {firstName} {lastName} would like to visit for{' '}
-      <Moment from={start} ago>
-        {end}
-      </Moment>{' '}
-      from <Moment format={'MMMM Do'}>{start}</Moment>
-      <IconButton color="secondary" onClick={() => onAccept(true)}>
-        <Icon>check</Icon>
-      </IconButton>
-      <IconButton color="default" onClick={() => onAccept(false)}>
-        <Icon>close</Icon>
-      </IconButton>
-    </Paper>
+    <RequestCard>
+      <RequestContainer>
+        <HeaderLink to={`/profile/${_id}`}>
+          {firstName} {lastName}
+        </HeaderLink>{' '}
+        would like to visit for{' '}
+        <Moment from={start} ago>
+          {end}
+        </Moment>{' '}
+        from <Moment format={'MMMM Do'}>{start}</Moment>
+      </RequestContainer>
+      <AuthorPicBox>
+        <HeaderLink to={`/profile/${_id}`}>
+          <AuthorPic>
+            <img src={profilePicture} style={{ width: 65, height: 65, borderRadius: 180 }} alt="Card" />
+          </AuthorPic>
+        </HeaderLink>
+      </AuthorPicBox>
+      <ButtonContainer>
+        <IconButton color="secondary" onClick={() => onAccept(true)}>
+          <Icon>check</Icon>
+        </IconButton>
+        <IconButton color="default" onClick={() => onAccept(false)}>
+          <Icon>close</Icon>
+        </IconButton>
+      </ButtonContainer>
+      <DescriptionContainer>
+        <b>Message to host:</b> {description}
+      </DescriptionContainer>
+      <ContactContainer>
+        <p>
+          <b>Email:</b> {email}
+        </p>
+        <p>
+          <b>Phone:</b> {phoneNumber}
+        </p>
+      </ContactContainer>
+    </RequestCard>
   );
 };
 
